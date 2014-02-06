2560e3ed69430929db24cac94952c60a0f5e2a71
Miguel Castro <miguel.castro@axen.pro>
2014-02-06 12:28:25 -0600

    project files added

---
+++ HeadspringLib/DivNumber.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadspringLib
+{
+    public class DivNumber
+    {
+        public delegate void SendMessagesHandler(MessagesArgs msgArgs);
+        public SendMessagesHandler ViewMessages;
+
+        public List<string> IsDivisable(int start, int end)
+        {
+            List<string> messages = new List<string>();
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                     messages.Add("fizzbuzz");
+                else if (i % 3 == 0)
+                      messages.Add("fizz");
+                else if (i % 5 == 0)
+                     messages.Add("buzz");
+                else
+                    messages.Add(i.ToString());
+
+            }
+            return messages;
+        }
+
+        public void RunIsDivisable(int start, int end)
+        {
+            int index = 0;
+
+            List<string> messages = new List<string>();
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                    messages.Add("fizzbuzz");
+                else if (i % 3 == 0)
+                    messages.Add("fizz");
+                else if (i % 5 == 0)
+                    messages.Add("buzz");
+                else
+                    messages.Add(i.ToString());
+
+                //The event is raised every 1000 rows ans then it cleans the List object
+                index = i;
+                if (index % 1000 == 0)
+                {   
+                    MessagesArgs newMsg = new MessagesArgs() { messagesToShow = messages };
+                    ViewMessages(newMsg);
+                    messages.Clear();   
+                }
+            }
+
+            if (messages.Count > 0)
+            {
+                MessagesArgs newMssg = new MessagesArgs() { messagesToShow = messages };
+                ViewMessages(newMssg);
+            }
+        }
+
+    }
+
+
+    public class MessagesArgs : EventArgs
+    {
+        public List<string> messagesToShow { get; set; }
+    }
+}
---
+++ headspringExample1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HeadspringLib;
+
+namespace headspringExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 1; i < 100; i++)
+            //{
+            //    if (i % 3 == 0 && i % 5 == 0)
+            //        Console.WriteLine("fizzbuzz");
+            //    else if (i % 3 == 0)
+            //        Console.WriteLine("fizz");
+            //    else if (i % 5 == 0)
+            //        Console.WriteLine("buzz");
+            //    else
+            //        Console.WriteLine(i);
+            //}
+            HeadspringLib.DivNumber DivNum = new DivNumber();
+            
+            //foreach (string message in DivNum.IsDivisable(1, 100))
+            //{
+            //    Console.WriteLine(message);
+            //}
+            
+            DivNum.ViewMessages += new DivNumber.SendMessagesHandler(PrintMessages);
+            DivNum.RunIsDivisable(1, 10000234);
+
+            Console.ReadLine();
+        }
+
+        public static void PrintMessages(MessagesArgs listMsg)
+        {
+            foreach (string messg in listMsg.messagesToShow)
+            {
+                Console.WriteLine(messg);
+            }
+        }
+    }
+}
